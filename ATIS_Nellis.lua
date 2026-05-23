5ba509bdffd375d2d2d501b0420b0db34b30b535
Blue Storm <b04rdr1d3r@gmail.com>
2026-05-23 09:13:25 +0200

    minor changes and improvements
    adding Mirage 2000 / adding waypoints for F-14 / correcting ATIS

--- ATIS_Nellis.lua
+++ ATIS_Nellis.lua
@@ -2,7 +2,7 @@
 atisNellis = ATIS:New("Nellis", 144.0)
 atisNellis:SetTACAN(12)
 atisNellis:SetRadioRelayUnitName("Radio Relay Nellis")
-atisNellis:SetActiveRunway("21L")
+atisNellis:SetActiveRunway("03R")
 atisNellis:SetTowerFrequencies({327.000, 132.550})
 atisNellis:AddILS(109.1, "21")
 atisNellis:Start()
